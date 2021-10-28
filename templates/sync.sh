946aa1d29498bd031eafae9a19da9c38d3e1b919
Cristian Mazzetti Martin <cristian@kalexgroup.ltd>
2021-10-28 06:17:50 +0100

    update

---
+++ templates/sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -va --delete "/Users/cristian/Library/Application Support/SketchUp 2021/SketchUp/Templates/" "/Users/cristian/GitHub/standbuilder/StandBuilder.io/templates/sketchup/"
+
+rsync -va --delete "/Users/cristian/Library/Application Support/SketchUp 2021/Layout/Templates/" "/Users/cristian/GitHub/standbuilder/StandBuilder.io/templates/layout/"
