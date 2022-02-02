054380395b716f747fe1d1e416b673f22f050fbc
Cristian @ KALEX GROUP Ltd <cristian@kalexgroup.ltd>
2022-02-02 04:42:15 +0000

    update

---
+++ templates/sync_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -va --delete "/Users/cristian-kalex/GitHub/standbuilder/Design-Library/templates/sketchup/" "/Users/cristian-kalex/Library/Application Support/SketchUp 2021/SketchUp/Templates/" 
+
+rsync -va --delete "/Users/cristian-kalex/GitHub/standbuilder/Design-Library/templates/layout/" "/Users/cristian-kalex/Library/Application Support/SketchUp 2021/Layout/Templates/"
